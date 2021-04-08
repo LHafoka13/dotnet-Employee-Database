33c6fd4e777493ec2204b6ed825cac2947d454ca
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-04-08 15:29:23 -0600

    updating code to iterate over instances of Employee rather than strings

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace CatWorx.BadgeMaker
     class Program
     {
 
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
                 Console.WriteLine("Please enter a name: (leave empty to exit): ");
@@ -20,24 +20,24 @@ namespace CatWorx.BadgeMaker
                 //create a new Employee instance
                 Employee currentEmployee = new Employee(input, "Smith");
                 //adds the user input into the list, combining the first and last name as one as defined in Employee.cs
-                employees.Add(currentEmployee.GetName());
+                employees.Add(currentEmployee);
             }
             // This is important!
             return employees;
         }
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
 
-                Console.WriteLine(employees[i]);
+                Console.WriteLine(employees[i].GetName());
 
             }
         }
 
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
 
